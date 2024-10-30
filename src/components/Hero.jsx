54bc30e6f958526bcecad30aeb876ad658c63a05
sirconscious <157235204+sirconscious@users.noreply.github.com>
2024-10-30 21:00:08 +0100

    beforeDishes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -50,7 +50,7 @@ export default function Hero() {
             }}
             src={logo2} alt="" className=' p-4' />
 
-            <p className='p-4 text-lg tracking-tighter text-white'>Moroccoooooooo</p>
+            <p className='p-4 text-lg tracking-tighter text-white'>Marrakech</p>
 
          </div>
         </div>
