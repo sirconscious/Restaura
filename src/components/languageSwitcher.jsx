d98d4f308a7254f8a7d9b1b5ac6bdaf2f07203ce
abdessamieaksas <127380411+abdessamieaksas@users.noreply.github.com>
2024-11-11 20:54:39 +0100

    correcte language button in moblie

--- src/components/languageSwitcher.jsx
+++ src/components/languageSwitcher.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import flagEn from '../assets/kingdom-flag.jpg';
 import flagFr from '../assets/flag-france.jpg';
+import { div } from 'framer-motion/client';
 
 const LanguageSwitcher = () => {
   const { i18n } = useTranslation();
@@ -15,7 +16,7 @@ const LanguageSwitcher = () => {
   return (
     
 
-  <img  onClick={toggleLanguage}  src={i18n.language === 'en' ? flagEn : flagFr} alt="flag" className='sm:ml-4 sm:mb-4 rounded-md border-gray-500 border transition-all w-11 cursor-pointer  ' />
+  <div className='p-4 lg:p-0'><img  onClick={toggleLanguage}  src={i18n.language === 'en' ? flagEn : flagFr} alt="flag" className='sm:ml-4 sm:mb-4 rounded-md border-gray-500 border transition-all w-9 cursor-pointer  ' /></div>
 
 
 
